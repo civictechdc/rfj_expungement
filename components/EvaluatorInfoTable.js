378b29301da69bf72a6da301958d28f15b859860
Chris Mazzullo <chris.mazzullo@gmail.com>
2020-03-22 14:58:14 -0400

    Fixed linting issues

--- components/EvaluatorInfoTable.js
+++ components/EvaluatorInfoTable.js
@@ -2,32 +2,20 @@ import React, { useState, useContext, Fragment, useRef } from "react";
 import { CaseContext } from "../contexts/casecontroller";
 
 // mui
-import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 
 // inputs
-import { Button, FormControlLabel } from "@material-ui/core";
-import ComposedDatePicker from "./ComposedDatePicker.js";
 import TextField from "@material-ui/core/TextField";
-import Switch from "@material-ui/core/Switch";
 
-// components
-
-const useStyles = makeStyles(theme => ({
-  table: {}
-}));
-
-function EvaluatorInfoTable(props) {
-  const classes = useStyles();
+function EvaluatorInfoTable() {
   const value = useContext(CaseContext);
   const [name, setName] = useState(value.caseData.evaluatorName);
 
-  let timerRef = useRef( null );
+  let timerRef = useRef(null);
   let TIMEOUT_DURATION = 1000;
 
-  const persist = (name) => {
-    
+  const persist = name => {
     if (timerRef.current) {
       clearTimeout(timerRef.current);
       timerRef.current = null;
