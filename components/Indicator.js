a2b0baaac18119d0019d2dad361041a621d9bcd3
Chris Mazzullo <chris.mazzullo@gmail.com>
2020-04-04 21:34:55 -0400

    Added links to DC law library to analysis section

--- components/Indicator.js
+++ components/Indicator.js
@@ -16,7 +16,11 @@ function IndicatorSection(props) {
   return (
     <>
       <Grid item xs={3}>
-        <Typography variant="h6">{props.title}</Typography>
+        <Typography variant="h6">
+          <a href={props.link} target="_blank" rel="noopener noreferrer">
+            {props.title}
+          </a>
+        </Typography>
       </Grid>
       <Grid item xs={1}>
         {iconMap[props.section.eligible]}
@@ -32,10 +36,12 @@ function Indicator(props) {
   return (
     <Grid container spacing={0}>
       <IndicatorSection
+        link="https://code.dccouncil.us/dc/council/code/sections/16-802.html"
         title="16-802 (Actual Innocence)"
         section={props.analysis.section802}
       />
       <IndicatorSection
+        link="https://code.dccouncil.us/dc/council/code/sections/16-803.html"
         title="16-803 (Interest of Justice)"
         section={props.analysis.section803}
       />
